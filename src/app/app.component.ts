beb4987f5cbef9b9312cc16b59f73bdf0d8da7c2
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-05-07 13:56:05 -0500

    Se menú de navegación.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-avanzada';
+  footer = 'Universidad del Quindío';
 }
