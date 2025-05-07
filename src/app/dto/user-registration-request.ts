2acb80f663ecd0225e5bb8f1b23b682ad1c45a2e
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-05-07 14:45:52 -0500

    Se crean los dtos para el servicio.

---
+++ src/app/dto/user-registration-request.ts
@@ -0,0 +1,6 @@
+export interface UserRegistrationRequest {
+  email: string,
+  password: string,
+  fullName: string,
+  dateBirth: Date
+}
---
+++ src/app/dto/user-response.ts
@@ -0,0 +1,7 @@
+export interface UserResponse {
+  id: string,
+  email: string,
+  fullName: string,
+  dateBirth: Date,
+  rol: string
+}
