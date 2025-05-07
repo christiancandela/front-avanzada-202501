11fbcfb185b1d5bb85b092abeb6630e767be5e86
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-05-07 14:16:03 -0500

    Se crea el formulario y se asocia a los componentes web.

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  registroForm!: FormGroup;
   result = '';
+
+  constructor(private formBuilder: FormBuilder) {
+    this.crearFormulario();
+  }
+
+  private crearFormulario() {
+    this.registroForm = this.formBuilder.group({
+      fullName: ['', []],
+      email: ['', []],
+      dateBirth: ['', []],
+      password: ['', []]
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Datos del formulario:', this.registroForm.value);
+    this.result = 'Registro exitoso';
+  }
 }
