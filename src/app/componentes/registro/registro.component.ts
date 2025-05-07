3bd7e75f956727cc608b894abb958d52d05c942f
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-05-07 14:20:42 -0500

    Se adicionan validaciones.

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -17,15 +17,26 @@ export class RegistroComponent {
 
   private crearFormulario() {
     this.registroForm = this.formBuilder.group({
-      fullName: ['', []],
-      email: ['', []],
-      dateBirth: ['', []],
-      password: ['', []]
-    });
+        fullName: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        dateBirth: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(8)]],
+        confirmPassword: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(8)]]
+      },
+      {
+        validators: this.passwordMatchValidator
+      });
   }
 
   onSubmit(): void {
     console.log('Datos del formulario:', this.registroForm.value);
     this.result = 'Registro exitoso';
+  }
+
+  passwordMatchValidator(formGroup: FormGroup): any {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
+    return password === confirmPassword ? null : { mismatch: true };
   }
 }
