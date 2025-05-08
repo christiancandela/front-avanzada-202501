a0ca5d8b18012f3e7893fadb50fe509be8cc38dc
cacandela <christiancandela@gmail.com>
2025-05-08 11:14:32 -0500

    Se soluciona actividad de seguimiento.

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
 import {UserRegistrationRequest} from '../../dto/user-registration-request';
 import {UsersService} from '../../servicios/users.service';
 import {ErrorResponse} from '../../dto/error-response';
 
 @Component({
   selector: 'app-registro',
-  imports: [ReactiveFormsModule,NgIf],
+  imports: [ReactiveFormsModule, NgIf, NgClass],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
   registroForm!: FormGroup;
   result = '';
+  classResult = 'success';
 
   constructor(private formBuilder: FormBuilder, private usersService: UsersService) {
     this.crearFormulario();
@@ -38,6 +39,7 @@ export class RegistroComponent {
       next: ( data) => {
         console.log('El usuario ha sido creado correctamente: ', data);
         this.result = 'Usuario registrado correctamente';
+        this.classResult = 'success';
       },
       error: (error) => {
         console.log('Se presentó un problema al registrar el usuario: ', error);
@@ -46,7 +48,7 @@ export class RegistroComponent {
         } else {
           this.result = error.error.message;
         }
-
+        this.classResult = 'error';
       }
     });
   }
